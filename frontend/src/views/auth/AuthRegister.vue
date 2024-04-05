3a9b1424eace9143c55082b2ee62c31b20cb9c18
Sarah B.M <sarah.melin19403@gmail.com>
2024-04-05 12:37:55 +0200

    DEV-2 add logo

--- frontend/src/views/auth/AuthRegister.vue
+++ frontend/src/views/auth/AuthRegister.vue
@@ -1,30 +1,31 @@
 <template>
-  <div style="padding: 2rem">
+  <div id="registerPage">
+
+    <img src="../../assets/renotracker-logo-notext.png" width="150" class="renotracker-logo" alt="logo">
+
     <h2>{{ t('auth.registerTitle') }}</h2>
 
-    <input-custom v-model="form.name" 
-      :label="t('auth.nameField')" 
-      size="md" 
-      required>
-    </input-custom>
+    <form>
+      <input-custom v-model="form.name" 
+        :label="t('auth.nameField')"
+        required>
+      </input-custom>
 
-    <input-custom v-model="form.email" 
-      :label="t('auth.emailField')" 
-      size="md" 
-      required>
-    </input-custom>
+      <input-custom v-model="form.email" 
+        :label="t('auth.emailField')" 
+        required>
+      </input-custom>
 
-    <input-custom v-model="form.password" 
-      :label="t('auth.passwordField')" 
-      size="md" 
-      required>
-    </input-custom>
+      <input-custom v-model="form.password" 
+        :label="t('auth.passwordField')" 
+        required>
+      </input-custom>
 
-    <input-custom v-model="form.password_confirmation" 
-      :label="t('auth.passwordConfirmField')"
-      size="md" 
-      required>
-    </input-custom>
+      <input-custom v-model="form.password_confirmation" 
+        :label="t('auth.passwordConfirmField')"
+        required>
+      </input-custom>
+    </form>
   </div>
 </template>
 
@@ -43,7 +44,30 @@ let form = reactive({
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+#registerPage {
+  margin-top: 5rem;
+}
+
+.renotracker-logo {
+  position: absolute; 
+  top: .5rem; 
+  left: 1rem;
+}
+
+#registerPage h2 {
+  text-align: center;
+  margin-top: 0;
+  color: $primary;
+  font-weight: 400;
+}
+
+form {
+  margin: auto;
+  width: 35%;
+}
+
 .input-custom-group {
   margin-bottom: 1rem;
 }
