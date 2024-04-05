5c3c6895ab2927da81559f35baf99f32d03c11de
Sarah B.M <sarah.melin19403@gmail.com>
2024-04-05 12:16:27 +0200

    DEV-2 setup i18n + basic register form

---
+++ frontend/src/i18n/fr-FR/index.js
@@ -0,0 +1,12 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  auth: {
+    registerTitle: 'Créer un compte',
+    nameField: 'Nom',
+    emailField: 'Email',
+    passwordField: 'Mot de passe',
+    passwordConfirmField: 'Confirmation du mot de passe',
+  }
+}
--- frontend/src/views/auth/AuthRegister.vue
+++ frontend/src/views/auth/AuthRegister.vue
@@ -1,13 +1,50 @@
 <template>
   <div style="padding: 2rem">
-    <input-custom v-model="test" label="coucou" size="md" required></input-custom>
+    <h2>{{ t('auth.registerTitle') }}</h2>
+
+    <input-custom v-model="form.name" 
+      :label="t('auth.nameField')" 
+      size="md" 
+      required>
+    </input-custom>
+
+    <input-custom v-model="form.email" 
+      :label="t('auth.emailField')" 
+      size="md" 
+      required>
+    </input-custom>
+
+    <input-custom v-model="form.password" 
+      :label="t('auth.passwordField')" 
+      size="md" 
+      required>
+    </input-custom>
+
+    <input-custom v-model="form.password_confirmation" 
+      :label="t('auth.passwordConfirmField')"
+      size="md" 
+      required>
+    </input-custom>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { reactive } from "vue";
+import { useI18n } from "vue-i18n";
 import InputCustom from "../common/InputCustom.vue";
 
-let test = ref();
-console.log(test);
+const {t} = useI18n()
+
+let form = reactive({
+  name: "",
+  email: "",
+  password: "",
+  password_confirmation: "",
+});
 </script>
+
+<style scoped>
+.input-custom-group {
+  margin-bottom: 1rem;
+}
+</style>
