5e5264d2b5904ef920aedbb3f65dd494214264f4
Sarah - Promo Crusoe <44750278+blupblupdoux@users.noreply.github.com>
2024-04-15 09:52:09 +0200

    Dev 3 sidebar navigation (#2)
    * DEV-3 base layout + side navigation
    
    * DEV-3 i18n texts side navbar

---
+++ frontend/src/views/nav/NavSidebar.vue
@@ -0,0 +1,65 @@
+<template>
+<q-drawer
+  v-model="drawer"
+  show-if-above
+  :width="200"
+  :breakpoint="400"
+  
+  class="column justify-between"
+  style="box-shadow: -4px 0px 8px -2px #000000;"
+>
+
+  <div>
+    <nav-sidebar-header></nav-sidebar-header>
+    
+    <q-list padding>
+      <nav-sidebar-item v-for="item in mainItems" 
+        :key="'mainItem-' + item.label"
+        :item="item">
+      </nav-sidebar-item>
+    </q-list>
+
+    <div style="border-bottom: 1px solid rgba(0, 0, 0, 0.12)"></div>
+
+    <q-list padding>
+      <nav-sidebar-item v-for="item in specificsItems" 
+        :key="'specificsItems-' + item.label"
+        :item="item">
+      </nav-sidebar-item>
+    </q-list>
+  </div>
+  
+  <img src="../../assets/renotracker-logo-linear-trim.png" class="sidebar-logo" />
+</q-drawer>
+</template>
+
+<script setup>
+import NavSidebarHeader from './NavSidebarHeader.vue';
+import NavSidebarItem from './NavSidebarItem.vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n()
+
+const mainItems = [
+  {label: t('nav.projects'), link: "", icon: "r_handyman"},
+  {label: t('nav.tools'), link: "", icon: "r_build"},
+]
+
+const specificsItems = [
+  {label: t('nav.dashboard'), link: "", icon: "r_dashboard"},
+  {label: t('nav.subProjects'), link: "", icon: "chair"},
+  {label: t('nav.purchases'), link: "", icon: "shopping_cart"},
+  {label: t('nav.tags'), link: "", icon: "tag"},
+  {label: t('nav.stats'), link: "", icon: "r_signal_cellular_alt"},
+  {label: t('nav.settings'), link: "", icon: "r_settings"},
+]
+</script>
+
+<style scoped>
+.sidebar-logo {
+  width: 60%;
+  margin: 0 auto;
+  display: block;
+  margin-bottom: 1rem;
+}
+</style>
---
+++ frontend/src/views/nav/NavSidebarHeader.vue
@@ -0,0 +1,28 @@
+<template>
+
+<div id="SidebarHeader">
+  <div class="row items-center q-mb-sm">
+    <q-icon name="face" size="35px"></q-icon>
+    <div class="q-ml-sm text-weight-bold text-h6">Sarah B</div>
+  </div>
+
+  <router-link to="#" class="link">{{ t('nav.profile') }}</router-link>
+</div>
+
+</template>
+
+<script setup>
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n()
+</script>
+
+<style lang="scss" scoped>
+#SidebarHeader {
+  padding: 1.5rem 1rem;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+}
+
+.link {
+  font-size: 0.7rem;
+}
+</style>
