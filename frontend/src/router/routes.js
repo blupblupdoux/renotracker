d134fcd597cac3b75700fd5443f8317549277a1f
Sarah B.M <sarah.melin19403@gmail.com>
2024-04-17 11:55:12 +0200

    DEV-4 restructuration

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,15 +1,27 @@
-import authRoutes from './auth'
-
 const routes = [
 
-  ...authRoutes,
+  {
+    path: '/auth',
+    children: [
+      { path: 'login', component: () => import('../views/pages/AuthFormPage.vue') },
+      { path: 'register', component: () => import('../views/pages/AuthFormPage.vue') }
+    ]
+  },
+  {
+    path: '/user/profile',
+    component: () => import('../views/pages/UserProfile.vue')
+  },
+  // {
+  //   path: '/projects',
+  //   component: () => import('../views/user/UserProfile.vue')
+  // },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     name: 'notFound',
-    component: () => import('../views/ErrorNotFound.vue')
+    component: () => import('../views/pages/ErrorNotFoundPage.vue')
   }
 ]
 
