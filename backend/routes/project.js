ec5fbb672f0f4a953c347909b6f5b50a8bbf449d
Sarah B.M <sarah.melin19403@gmail.com>
2024-04-24 12:35:12 +0200

    DEV-4 get all projects endpoint front

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const projectController = require('../controllers/Project')
 
+router.get('/all', projectController.all)
 router.post('/create', projectController.create)
 
 module.exports = router
--- frontend/src/views/pages/ProjectsAll.vue
+++ frontend/src/views/pages/ProjectsAll.vue
@@ -3,5 +3,14 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
 import ProjectForm from '../project/ProjectForm.vue'
+import { api } from 'src/boot/axios';
+
+onMounted(() => {
+  api.get('/api/project/all')
+    .then(response => {
+      console.log(response.data)
+    })
+})
 </script>
